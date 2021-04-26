d00066fb787af02380fdc5cf6670548ca97f260e
Maxim Kovrov <maksim.kovrov@gmail.com>
2021-04-26 23:13:57 +0300

    step 01

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cured-plumbum/philatelist
 
 go 1.15
+
+require github.com/rs/zerolog v1.21.0
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/rs/zerolog/log"
+)
+
+// https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyC3MrrX8-db5JZW2_LwhwsjN1yXjdkj5YQ
+
+const placeTextSearch = "https://maps.googleapis.com/maps/api/place/textsearch/json"
 
 func main() {
-	fmt.Println("Hello, world!")
+	uq := make(url.Values)
+
+	uq.Set("key", "AIzaSyC3MrrX8-db5JZW2_LwhwsjN1yXjdkj5YQ")
+	uq.Set("input", "Москва, Тверская, 14")
+
+	reqURL := placeTextSearch + "?" + uq.Encode()
+
+	log.Info().Msg(reqURL)
+
+	res, err := http.Get(reqURL)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Невозможно получить места")
+	}
+	defer res.Body.Close()
+
+	bb, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Невозможно считать ответ из ответа")
+	}
+
+	fmt.Fprintln(os.Stdout, string(bb))
 }
