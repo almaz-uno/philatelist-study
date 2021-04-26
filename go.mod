b55d1296aef20a1949c611741e5e611bbd2d6fd6
Maxim Kovrov <maksim.kovrov@gmail.com>
2021-04-26 23:39:08 +0300

    step 02

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/cured-plumbum/philatelist
 
 go 1.15
 
-require github.com/rs/zerolog v1.21.0
+require (
+	github.com/kr/pretty v0.2.1
+	github.com/rs/zerolog v1.21.0
+)
--- main.go
+++ main.go
@@ -1,39 +1,77 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
 
+	"github.com/kr/pretty"
 	"github.com/rs/zerolog/log"
 )
 
 // https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyC3MrrX8-db5JZW2_LwhwsjN1yXjdkj5YQ
 
+type (
+	queryPlaceResponse struct {
+		Results []struct {
+			PlaceID string `json:"place_id"`
+		} `json:"results"`
+	}
+
+	placeAPI struct {
+		key  string
+		lang string
+	}
+)
+
 const placeTextSearch = "https://maps.googleapis.com/maps/api/place/textsearch/json"
 
-func main() {
-	uq := make(url.Values)
+func (a *placeAPI) doGet(reqURL string, query url.Values) ([]byte, error) {
+	if query == nil {
+		query = make(url.Values)
+	}
 
-	uq.Set("key", "AIzaSyC3MrrX8-db5JZW2_LwhwsjN1yXjdkj5YQ")
-	uq.Set("input", "Москва, Тверская, 14")
+	query.Set("key", a.key)
+	query.Set("language", a.lang)
 
-	reqURL := placeTextSearch + "?" + uq.Encode()
-
-	log.Info().Msg(reqURL)
-
-	res, err := http.Get(reqURL)
+	res, err := http.Get(reqURL + "?" + query.Encode())
 	if err != nil {
-		log.Fatal().Err(err).Msg("Невозможно получить места")
+		return nil, fmt.Errorf("unable to do request to Google: %w", err)
 	}
 	defer res.Body.Close()
 
 	bb, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatal().Err(err).Msg("Невозможно считать ответ из ответа")
+		return nil, fmt.Errorf("unable to read request body from Google: %w", err)
 	}
+
+	return bb, nil
+}
+
+func main() {
+	address := "Москва, Тверская, 14"
+
+	api := &placeAPI{
+		key:  "AIzaSyC3MrrX8-db5JZW2_LwhwsjN1yXjdkj5YQ",
+		lang: "ru",
+	}
+
+	bb, err := api.doGet(placeTextSearch, url.Values{"input": []string{address}})
+	if err != nil {
+		log.Fatal().Err(err).Msg("Ошибка обращения в Google")
+	}
+
+	qr := &queryPlaceResponse{}
+
+	err = json.Unmarshal(bb, qr)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Невозможно демаршализовать JSON из ответа Google")
+	}
+
+	pretty.Println(qr)
 
 	fmt.Fprintln(os.Stdout, string(bb))
 }
